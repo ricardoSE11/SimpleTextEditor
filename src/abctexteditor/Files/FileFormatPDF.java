a9896e62ae85bb47ebeed6ddc1572ee3079f76db
jusola2 <juanjosesolano@hotmail.com>
2019-05-24 17:38:23 -0600

    Update

--- src/abctexteditor/Files/FileFormatPDF.java
+++ src/abctexteditor/Files/FileFormatPDF.java
@@ -73,7 +73,7 @@ public class FileFormatPDF implements IFileFormat{
 
     @Override
     public String removeFormat(String fileContent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return fileContent;
     }
 
     
